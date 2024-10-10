0bb5c85371caaf20ec1205f9c551125ed4c11a8b
IHIMBAZWE <ihimbazwegenevieve123@gmail.com>
2024-10-10 20:57:17 +0200

    Delete work2.py

--- work2.py
+++
@@ -1,9 +0,0 @@
-#average =total sum/number of item
-daily =[100,200,300,400,500,600,700]
-summation = sum(daily)
-average = summation/7
-print(average) 
-minimum=min(daily)
-print(minimum)
-maximum=max(daily)
-print(maximum)
