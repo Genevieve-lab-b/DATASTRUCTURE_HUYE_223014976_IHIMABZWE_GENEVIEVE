d951ef2ffba3b492fb457b1c508d34f06f4eba45
IHIMBAZWE <ihimbazwegenevieve123@gmail.com>
2024-10-10 20:58:15 +0200

    Delete work.py

--- work.py
+++
@@ -1,5 +0,0 @@
-temperatures =[10,20,30,40,50,60,70]
-for temperature in temperatures:
-    print(temperature)
-    for index, temperature in enumerate (temperatures):
-        print( 'day',index+1,temperature,temperature)
